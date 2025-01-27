7e6b4065f588802ea449a89b65f5663901699a79
Arnau <arnau.roca@inslapineda.cat>
2025-01-27 11:51:16 +0100

    Primer edit pagina inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,65 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi Tienda Online</title>
+    <title>Sneaks</title>
 </head>
 <body>
-    <h1>Bienvenido a Mi Tienda Online</h1>
-    <p>Explora nuestros productos y servicios.</p>
+    <div class="nav">
+        <div class="foto">
+            <img src="" alt="">
+        </div>
+        <div class="buscador">
+
+        </div>
+        <div class="usuario">
+            <img src="" alt="UserFoto">
+            <p>Nombre usuario</p>
+        </div>
+    </div>
+
+    <img src="" alt="">
+
+    <div class="marcasInicio">
+        <div class="marcaInicio">
+            <img src="" alt="">
+            <p>Nike</p>
+        </div>
+
+        <div class="marcaInicio">
+            <img src="" alt="">
+            <p>Adidas</p>
+        </div>
+
+        <div class="marcaInicio">
+            <img src="" alt="">
+            <p>Jordan</p>
+        </div>
+
+        <div class="marcaInicio">
+            <img src="" alt="">
+            <p>Yeezy</p>
+        </div>
+
+        <div class="marcaInicio">
+            <img src="" alt="">
+            <p>New Balance</p>
+        </div>
+
+        <a href=""><button>VER TODAS</button></a>
+    </div>
+
+    <footer>
+        <div>
+
+        </div>
+
+        <div>
+            
+        </div>
+
+        <div>
+            
+        </div>
+    </footer>
 </body>
 </html>
