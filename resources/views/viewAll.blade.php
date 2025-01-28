12a5eff13662d2dad1a10c8473daef8d572df828
marcmartin27 <marc.martin@inslapineda.cat>
2025-01-28 09:47:53 +0100

    Creacion de vistas nuevas y scss

--- resources/views/viewAll.blade.php
+++ resources/views/viewAll.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sneaks</title>
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 </head>
 <body>
     <div class="nav">
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('login');
 });
+
+Route::get('/viewall', function () {
+    return view('viewAll');
+});
