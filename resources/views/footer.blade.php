c2e98f95db136ee027a5a64cc454650af2285024
Arnau <arnau.roca@inslapineda.cat>
2025-01-31 08:54:41 +0100

    Inicio scss footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,14 +1,14 @@
 <footer>
-    <div>
+    <div class="texto">
         <p>Visita nuestra pagina corporativa en www.sneaks.com</p>
         <p>Copyright © 2025 Sneaks, Todos los derechos reservados.</p>
     </div>
 
-    <div>
+    <div class="imagen">
         <img src="../../../images/logo_redondo.png" alt="">
     </div>
 
-    <div>
+    <div class="texto">
         <p>Aviso legal</p>
         <p>Politica de privacidad | Politica de cookies | Terminos y condiciones</p>
     </div>
